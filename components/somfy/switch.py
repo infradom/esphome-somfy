50522d8d3cae06103037b8dcb3c9a78ad43f4c44
Jan C <11804014+infradom@users.noreply.github.com>
2025-02-23 17:31:05 +0100

    Update switch.py

--- components/somfy/switch.py
+++ components/somfy/switch.py
@@ -2,10 +2,10 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
 
-empty_switch_ns = cg.esphome_ns.namespace("empty_switch")
-EmptySwitch = empty_switch_ns.class_("EmptySwitch", switch.Switch, cg.Component)
+somfy_switch_ns = cg.esphome_ns.namespace("somfy_switch")
+SomfySwitch = somfy_switch_ns.class_("SomfySwitch", switch.Switch, cg.Component)
 
-CONFIG_SCHEMA = switch.switch_schema(EmptySwitch).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = switch.switch_schema(SomfySwitch).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
