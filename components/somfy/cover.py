101663d2298e12c3c7dbce9a1bfbc3d8e7d8679b
Jan C <11804014+infradom@users.noreply.github.com>
2025-02-23 18:27:42 +0100

    Update cover.py

--- components/somfy/cover.py
+++ components/somfy/cover.py
@@ -2,8 +2,10 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import cover
 from esphome.const import CONF_ID
+from esphome.const import CONF_DEVICE_CLASS #jco
+from esphome.components.cover import DEVICE_CLASSES # jco
 
-CONF_INITIAL_ROLLING_CODE  = "initial_rcode"
+CONF_INITIAL_ROLLING_CODE  = "initial_rolling_code"
 
 somfy_cover_ns = cg.esphome_ns.namespace("somfy_cover")
 SomfyCover = somfy_cover_ns.class_("SomfyCover", cover.Cover, cg.Component)
@@ -11,6 +13,7 @@ SomfyCover = somfy_cover_ns.class_("SomfyCover", cover.Cover, cg.Component)
 CONFIG_SCHEMA = cover.COVER_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(SomfyCover),
+        cv.Optional(CONF_DEVICE_CLASS): cv.one_of(*DEVICE_CLASSES, lower=True),
         cv.Optional(CONF_INITIAL_ROLLING_CODE, default=1): cv.int_,
     }
 ).extend(cv.COMPONENT_SCHEMA)
